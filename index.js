da046ba75e80e77de274b3fcb480dc8b1876d997
Carl <co398@kent.ac.uk>
2024-09-29 02:05:25 +0100

    registration and login page

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to Your Application!</h1>
-        <p>To get started, register or login to access the platform.</p>
-      </div>
-    );
-  }
-  
